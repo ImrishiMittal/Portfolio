0b6805cca3dcc0b44c2f62d5544c478d23e5b173
Rishi Mitttal <rishi11mital@gmail.com>
2026-06-16 21:18:24 +0530

    Added glass effect

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,52 @@
 import { Code2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
+import { useEffect, useState } from "react";
 import navLinks from "./NavLinks";
 import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+  
+    window.addEventListener("scroll", handleScroll);
+  
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <header className="fixed top-0 left-0 z-50 w-full">
+    <header
+  className={`
+    fixed
+    top-0
+    left-0
+    z-50
+    w-full
+
+    transition-all
+    duration-300
+
+    ${
+      scrolled
+        ? `
+          bg-black/40
+
+          backdrop-blur-xl
+
+          border-b
+
+          border-white/10
+        `
+        : `
+          bg-transparent
+        `
+    }
+  `}
+>
       <nav className="mx-auto flex h-16 max-w-7xl items-center justify-between px-6 md:h-20">
 
         {/* Logo */}
